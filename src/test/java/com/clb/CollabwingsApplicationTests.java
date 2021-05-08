b2e09060c57373709e0590d665f01e45fa6b71bc
Chijeong In <cde0409@naver.com>
2021-05-08 15:28:36 +0900

    Initailize project

---
+++ src/test/java/com/clb/CollabwingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabwingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
